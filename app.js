115e518ccf245e3f4ebe74f565cdd3db26d75dfa
Connor Flynn <connorflynn4@gmail.com>
2019-08-31 17:02:48 -0400

    show path in a breadcrumb

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+
+//require node modules
 const http = require('http');
 
 //file imports
 const respond = require('./lib/respond.js');
 
 //connection settings
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-
-//create server
+//Create server
 const server = http.createServer(respond);
 
-//listen to client requests
-server.listen(port,() => {
+//listen to client requests on the specific port, the port should be available
+server.listen(port, () => {
     console.log(`listening on port: ${port}`);
 });
